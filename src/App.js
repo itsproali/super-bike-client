12ffe04db7fa73a1201288c30a0efee5f9c90669
Mohammad Ali <itsproali@gmail.com>
2022-04-29 10:56:25 +0600

    Navbar Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import Home from "./comoponents/Home/Home";
+import Home from "./components/Home/Home";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div>
+      <Header></Header>
       <Routes>
         <Route path="/" element={<Home />}></Route>
       </Routes>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,7 +3,6 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h1>I am from Home</h1>
         </div>
     );
 };
