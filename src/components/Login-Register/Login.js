4e2f0e3133d163515b33b455c720995f86315a34
Mohammad Ali <itsproali@gmail.com>
2022-04-30 03:08:59 +0600

    Social Login Added

--- src/components/Login-Register/Login.js
+++ src/components/Login-Register/Login.js
@@ -3,10 +3,11 @@ import {
   useSendPasswordResetEmail,
   useSignInWithEmailAndPassword,
 } from "react-firebase-hooks/auth";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import auth from "../../firebase-init";
 import Spinner from "../Spinner/Spinner";
 import toast from "react-hot-toast";
+import SocialLogin from "../SocialLogin/SocialLogin";
 
 const Login = () => {
   const navigate = useNavigate();
@@ -40,8 +41,6 @@ const Login = () => {
       } else {
         toast.error("Something went Wrong");
       }
-    } else if (!resetError) {
-      toast.success("Password reset email sent");
     }
   }, [error, resetError]);
 
@@ -57,62 +56,76 @@ const Login = () => {
   const resetPassword = () => {
     if (email) {
       sendPasswordResetEmail(email);
+      toast.success("Password reset email sent");
     }
   };
 
   return (
-    <div className="form-container">
-      <form onSubmit={handleLogin}>
-        <div className="relative z-0 my-10">
-          <input
-            type="email"
-            name="email"
-            id="floating_standard"
-            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-            onBlur={(e) => setEmail(e.target.value)}
-            placeholder=" "
-            required
-          />
-          <label
-            htmlFor="floating_standard"
-            className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-          >
-            Email
-          </label>
-        </div>
+    <div>
+      <h1 className="text-3xl text-center text-blue-600 my-10 font-semibold">
+        Login
+      </h1>
+      <div className="form-container">
+        <form onSubmit={handleLogin}>
+          <div className="relative z-0 my-10">
+            <input
+              type="email"
+              name="email"
+              id="floating_standard"
+              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              onBlur={(e) => setEmail(e.target.value)}
+              placeholder=" "
+              required
+            />
+            <label
+              htmlFor="floating_standard"
+              className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Email
+            </label>
+          </div>
 
-        <div className="relative z-0 mt-10">
-          <input
-            type="password"
-            name="password"
-            id="floating_standard"
-            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-            onBlur={(e) => setPassword(e.target.value)}
-            placeholder=" "
-            required
-          />
-          <label
-            htmlFor="floating_standard"
-            className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-          >
-            Password
-          </label>
-        </div>
-        <div className="text-right  my-4">
-          <button
-            onClick={resetPassword}
-            className="text-blue-600 hover:underline"
-          >
-            Forgotten Password?
-          </button>
-        </div>
+          <div className="relative z-0 mt-10">
+            <input
+              type="password"
+              name="password"
+              id="floating_standard"
+              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              onBlur={(e) => setPassword(e.target.value)}
+              placeholder=" "
+              required
+            />
+            <label
+              htmlFor="floating_standard"
+              className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Password
+            </label>
+          </div>
+          <div className="text-right  my-4">
+            <button
+              onClick={resetPassword}
+              className="text-blue-600 hover:underline"
+            >
+              Forgotten Password?
+            </button>
+          </div>
 
-        <input
-          type="submit"
-          className="cursor-pointer w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-3 text-center duration-300"
-          value="Login"
-        />
-      </form>
+          <input
+            type="submit"
+            className="cursor-pointer w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-3 text-center duration-300"
+            value="Login"
+          />
+          <p className="text-center my-4">
+            Don't have an account?{" "}
+            <Link className="underline text-blue-600" to="/register">
+              Register Now
+            </Link>
+          </p>
+        </form>
+
+        <SocialLogin />
+      </div>
     </div>
   );
 };
---
+++ src/components/SocialLogin/SocialLogin.js
@@ -0,0 +1,74 @@
+import React, { useEffect } from "react";
+import {
+  useSignInWithFacebook,
+  useSignInWithGithub,
+  useSignInWithGoogle,
+} from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
+import auth from "../../firebase-init";
+import "./SocialLogin.css";
+import toast from "react-hot-toast";
+import Spinner from "../Spinner/Spinner";
+
+const SocialLogin = () => {
+  const navigate = useNavigate();
+
+  const [singInWithGoogle, googleUser, googleLoading, googleError] =
+    useSignInWithGoogle(auth);
+  const [signInWithFacebook, fbUser, fbLoading, fbError] =
+    useSignInWithFacebook(auth);
+  const [signInWithGithub, githubUser, githubLoading, githubError] =
+    useSignInWithGithub(auth);
+
+  useEffect(() => {
+    if (googleUser || fbUser || githubUser) {
+      navigate("/");
+    }
+  }, [googleUser, fbUser, githubUser, navigate]);
+
+  useEffect(() => {
+    if (googleError) {
+      toast.error(`${googleError.message}`);
+    } else if (fbError) {
+      toast.error(`${fbError.message}`);
+    } else if (githubError) {
+      toast.error(`${githubError.message}`);
+    }
+  }, [googleError, fbError, githubError]);
+
+  if (googleLoading || fbLoading || githubLoading) {
+    return <Spinner />;
+  }
+  return (
+    <div className="mt-6">
+      <div className="or-container">
+        <div className="or-line"></div>
+        <p className="text-gray-600">Or</p>
+        <div className="or-line"></div>
+      </div>
+      <button
+        className="social-btn google-btn"
+        onClick={() => singInWithGoogle()}
+      >
+        <img src="https://i.ibb.co/s3GpLz5/google-logo.png" alt="Google" />
+        <p className="google-text">Continue with Google</p>
+      </button>
+      <button
+        className="social-btn fb-btn"
+        onClick={() => signInWithFacebook()}
+      >
+        <img src="https://i.ibb.co/HKCqP2d/facebook-new.png" alt="Facebook" />
+        <p className="fb-text">Continue with Facebook</p>
+      </button>
+      <button
+        className="social-btn github-btn"
+        onClick={() => signInWithGithub()}
+      >
+        <img src="https://i.ibb.co/KqGjRQt/github.png" alt="GitHub" />
+        <p className="github-text">Continue with GitHub</p>
+      </button>
+    </div>
+  );
+};
+
+export default SocialLogin;
